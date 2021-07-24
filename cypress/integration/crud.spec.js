9b206f73f9c5b7aeae80917c9598e9d909af995f
Gisele Rosa <giselersantos.ti@gmail.com>
2021-07-24 19:32:52 -0300

    🚧 refactoring crud tests

--- cypress/integration/crud.spec.js
+++ cypress/integration/crud.spec.js
@@ -1,36 +1,16 @@
 describe('Notes', () => {
   beforeEach(() => {
-    cy.visit('http://notes-serverless-app.com')
-
-    cy.get('.navbar-nav a:contains(Login)').click()
-
-    cy.get('#email').type(Cypress.env('user'))
-    cy.get('#password').type(Cypress.env('password'))
-    cy.get('button[type="submit"]').click()
+    cy.login()
   })
 
-  it('creates a note', () => {
-    cy.contains('Create a new note').click()
+  it('create , edit and delete a note', () => {
+    cy.createNote()
+    cy.validateCreatedNote()
+ 
+    cy.editNote()
+    cy.validateEditNote()   
 
-    cy.get('#content').type('My note')
-    cy.contains('Create').click()
-
-    cy.get('.list-group').should('contain', 'My note')
-  })
-
-  it('edits a note', () => {
-    cy.get('.list-group').contains('My note').click()
-    cy.get('#content').type(' updated')
-    cy.contains('Save').click()
-
-    cy.get('.list-group').should('contain', 'My note updated')
-    cy.get('.list-group:contains(My note updated)').should('be.visible')
-  })
-
-  it('deletes a note', () => {
-    cy.get('.list-group').contains('My note updated').click()
-    cy.contains('Delete').click()
-
-    cy.get('.list-group:contains(My note updated)').should('not.exist')
+    cy.deleteCreatedNote()
+    cy.validateNoteNotExist()
   })
 })
