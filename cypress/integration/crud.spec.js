324200d359c98dd9b94008fd325ecda9ae5e976c
Otávio Landim <otavio.landim@gmail.com>
2021-11-30 16:55:34 -0300

    code review

--- cypress/integration/crud.spec.js
+++ cypress/integration/crud.spec.js
@@ -1,21 +1,26 @@
-import locators from "../commons/locators";
+import { NOTES } from "../commons/locators";
 
 describe('Notes', () => {
   beforeEach(() => {
     cy.visit('/')    
-    cy.loginAs()
+    cy.login()
     cy.url().should('be.equal', `${Cypress.config('baseUrl')}/login`)
   })
 
-  it('should validate crud flow successfully', () => {    
-    cy.createANote();
-    cy.get(locators.NOTES.LIST_GROUP).should('contain', 'My note');
+  it('should validate crud flow successfully', () => { 
+    const text = {
+      nameNote: 'My note',
+      nameNoteUpdated: 'My note updated'
+    } 
 
-    cy.editANote();
-    cy.get(locators.NOTES.LIST_GROUP).should('contain', 'My note updated');
-    cy.get(locators.NOTES.LIST_GROUP_UPDATED).should('be.visible');
+    cy.createNote(text);
+    cy.get(NOTES.LIST_GROUP).should('contain', text.nameNote);
 
-    cy.deleteANote();
-    cy.get(locators.NOTES.LIST_GROUP_UPDATED).should('not.exist');
+    cy.editNote(text);
+    cy.get(NOTES.LIST_GROUP).should('contain', text.nameNoteUpdated);
+    cy.get(NOTES.LIST_GROUP_UPDATED).should('be.visible');
+
+    cy.deleteNote(text);
+    cy.get(NOTES.LIST_GROUP_UPDATED).should('not.exist');
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -13,6 +13,33 @@
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
 //
+
+import { LOGIN, NOTES } from '../commons/locators';
+
+Cypress.Commands.add('login', (email = Cypress.env('email'), password = Cypress.env('password')) => {    
+    cy.get(LOGIN.LINK_LOGIN).click()
+    cy.get(LOGIN.EMAIL).type(email)
+    cy.get(LOGIN.PASSWORD).type(password, { log: false })
+    cy.get(LOGIN.BTN_LOGIN).click()
+})
+
+Cypress.Commands.add('createNote', (text) => {
+    cy.contains(NOTES.BTN_NEW_NOTE).click()
+    cy.get(NOTES.TEXT_AREA).type(text.nameNote)
+    cy.contains(NOTES.BTN_CREATE_NEW_NOTE).click()
+})
+
+Cypress.Commands.add('editNote', (text) => {
+    cy.get(NOTES.LIST_GROUP).contains(text.nameNote).click()
+    cy.get(NOTES.TEXT_AREA).clear().type(text.nameNoteUpdated)
+    cy.contains(NOTES.BTN_SAVE_UPDATE_NOTE).click()
+})
+
+Cypress.Commands.add('deleteNote', (text) => {
+    cy.get(NOTES.LIST_GROUP).contains(text.nameNoteUpdated).click()
+    cy.contains(NOTES.BTN_DELETE_NOTE).click()
+})
+
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
 //
@@ -24,28 +51,3 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import locators from '../commons/locators';
-
-Cypress.Commands.add('loginAs', (email = Cypress.env('email'), password = Cypress.env('password')) => {    
-    cy.get(locators.LOGIN.LINK_LOGIN).click()
-    cy.get(locators.LOGIN.EMAIL).type(email)
-    cy.get(locators.LOGIN.PASSWORD).type(password, { log: false })
-    cy.get(locators.LOGIN.BTN_LOGIN).click()
-})
-
-Cypress.Commands.add('createANote', () => {
-    cy.contains(locators.NOTES.BTN_NEW_NOTE).click()
-    cy.get(locators.NOTES.TEXT_AREA).type('My note')
-    cy.contains(locators.NOTES.BTN_CREATE_NEW_NOTE).click()
-})
-
-Cypress.Commands.add('editANote', () => {
-    cy.get(locators.NOTES.LIST_GROUP).contains('My note').click()
-    cy.get(locators.NOTES.TEXT_AREA).type(' updated')
-    cy.contains(locators.NOTES.BTN_SAVE_UPDATE_NOTE).click()
-})
-
-Cypress.Commands.add('deleteANote', () => {
-    cy.get(locators.NOTES.LIST_GROUP).contains('My note updated').click()
-    cy.contains(locators.NOTES.BTN_DELETE_NOTE).click()
-})
